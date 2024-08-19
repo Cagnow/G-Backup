586c00ee89279b83d1f56b0935fe25bc5e8b16eb
cagnoo <36724003+Cagnow@users.noreply.github.com>
2024-08-19 14:01:58 +0200

    Update G_Backup.py
    added zip handle

--- G_Backup.py
+++ G_Backup.py
@@ -1,10 +1,19 @@
 import os
 import sys
 import time
+import zipfile
 from datetime import datetime
 from google.oauth2 import service_account
 from googleapiclient.discovery import build
 from googleapiclient.http import MediaFileUpload
+
+def zip_folder(folder_path, output_path):
+    with zipfile.ZipFile(output_path, 'w', zipfile.ZIP_DEFLATED) as zipf:
+        for root, dirs, files in os.walk(folder_path):
+            for file in files:
+                file_path = os.path.join(root, file)
+                arcname = os.path.relpath(file_path, folder_path)
+                zipf.write(file_path, arcname)
 
 def upload_to_google_drive(file_path, credentials_path):
     # Authentification avec Google Drive
@@ -14,31 +23,9 @@ def upload_to_google_drive(file_path, credentials_path):
 
     # Ajout d'un horodatage au nom du fichier
     timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
-    file_name_with_timestamp = f"{os.path.basename(file_path)}_{timestamp}"
+    file_name_with_timestamp = f"{os.path.basename(file_path)}_{timestamp}.zip"
     file_metadata = {'name': file_name_with_timestamp}
-    media = MediaFileUpload(file_path, mimetype='application/octet-stream')
+    media = MediaFileUpload(file_path, mimetype='application/zip')
 
     # Upload du fichier sur Google Drive
-    file = service.files().create(body=file_metadata, media_body=media, fields='id').execute()
-    print(f"File uploaded successfully! File ID: {file.get('id')}")
-
-if __name__ == "__main__":
-    if len(sys.argv) != 3:
-        print("Usage: python script.py <path_to_file> <path_to_credentials>")
-        sys.exit(1)
-
-    file_path = sys.argv[1]
-    credentials_path = sys.argv[2]
-
-    if not os.path.exists(file_path):
-        print(f"The file {file_path} does not exist.")
-        sys.exit(1)
-
-    if not os.path.exists(credentials_path):
-        print(f"The credentials file {credentials_path} does not exist.")
-        sys.exit(1)
-
-    while True:
-        upload_to_google_drive(file_path, credentials_path)
-        print("Waiting for 30 minutes before the next backup...")
-        time.sleep(1800)  # Attendre 30 minutes (1800 secondes)
+    file = service.files().cr

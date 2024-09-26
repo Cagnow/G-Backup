f9d9cfd999303168c4b222f729befb7933ee5139
cagnoo <36724003+Cagnow@users.noreply.github.com>
2024-09-26 10:47:28 +0200

    Update G_Backup.py

--- G_Backup.py
+++ G_Backup.py
@@ -16,16 +16,11 @@ def zip_folder(folder_path, output_path):
                 zipf.write(file_path, arcname)
 
 def upload_to_google_drive(file_path, credentials_path):
-    # Authentification avec Google Drive
     SCOPES = ['https://www.googleapis.com/auth/drive.file']
     creds = service_account.Credentials.from_service_account_file(credentials_path, scopes=SCOPES)
     service = build('drive', 'v3', credentials=creds)
-
-    # Ajout d'un horodatage au nom du fichier
     timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
     file_name_with_timestamp = f"{os.path.basename(file_path)}_{timestamp}.zip"
     file_metadata = {'name': file_name_with_timestamp}
     media = MediaFileUpload(file_path, mimetype='application/zip')
-
-    # Upload du fichier sur Google Drive
     file = service.files().cr
